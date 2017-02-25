adb13e07c62b8bc768563ab8d9d8f0640f529200
Sébastien Vercammen <contact@sebastienvercammen.be>
2017-02-25 23:37:42 +0100

    Added super args.

--- pogom/extra/utilclasses.py
+++ pogom/extra/utilclasses.py
@@ -5,8 +5,8 @@ class StoppableThread(threading.Thread):
     """Thread class with a stop() method. The thread itself has to check
     regularly for the stopped() condition."""
 
-    def __init__(self):
-        super(StoppableThread, self).__init__()
+    def __init__(self, *args, **kwargs):
+        super(StoppableThread, self).__init__(*args, **kwargs)
         self._stop = threading.Event()
 
     def stop(self):
